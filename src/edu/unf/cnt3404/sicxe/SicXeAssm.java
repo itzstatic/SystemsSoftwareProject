f6151a8a0ff7c94c383b0ffa4280cb55a7c8b748
Brandon Mathis <mathisbb@gmail.com>
2017-10-26 22:04:03 -0400

    Wrote main method

--- src/edu/unf/cnt3404/sicxe/SicXeAssm.java
+++ src/edu/unf/cnt3404/sicxe/SicXeAssm.java
@@ -1,6 +1,9 @@
 package edu.unf.cnt3404.sicxe;
 
 import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.List;
@@ -18,16 +21,6 @@ public class SicXeAssm {
 	private Parser parser;
 	private Program program = new Program();
 	private List<Command> commands = new ArrayList<>();
-	
-	public static void main(String[] args) {
-		//Ensure there is one argument (file name)
-		
-		//Ensure file exists with the name
-		
-		//Parse file into syntax tree, consisting of pass one
-		
-		//Run pass two, creating .lst and .obj file
-	}
 	
 	public SicXeAssm(BufferedReader reader) {
 		parser = new Parser(reader);
@@ -61,5 +54,43 @@ public class SicXeAssm {
 			}
 		}
 		//Create end record
+	}
+	
+	public static void main(String[] args) {
+		//Ensure there is one argument (file name)
+		if (args.length < 1) {
+			System.err.println("Requires file name!");
+			System.exit(1);
+		}
+		
+		//Ensure file exists with the name
+		String fileName = args[0];
+		BufferedReader file = null;
+		try {
+			file = new BufferedReader(new FileReader(fileName));
+		} catch (FileNotFoundException e) {
+			System.err.println("Cannot open file '" + fileName + "'!");
+			System.exit(2);
+		}
+		//Do pass one
+		SicXeAssm assm = new SicXeAssm(file);
+		assm.passOne();
+		//Open .lst and .obj files
+		PrintWriter lst = null;
+		try {
+			lst = new PrintWriter(fileName + ".lst");
+		} catch (FileNotFoundException e) {
+			System.err.println("Cannot create listing file!");
+			System.exit(3);
+		}
+		PrintWriter obj = null;
+		try {
+			obj = new PrintWriter(fileName + ".obj");
+		} catch (FileNotFoundException e) {
+			System.err.println("Cannot create object file!");
+			System.exit(4);
+		}
+		//Run pass two, creating .lst and .obj file
+		assm.passTwo(lst, obj);
 	}
 }
